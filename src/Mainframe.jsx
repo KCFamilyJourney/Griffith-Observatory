88b7ea9030ee65cc4e9c8e62c9004518fdfe4e49
bobkkblue <chienyec@gmail.com>
2025-12-24 15:48:42 -0800

    update

--- src/Mainframe.jsx
+++ src/Mainframe.jsx
@@ -10,11 +10,18 @@ function Mainframe(){
     const choices = [ 
         {   name: "Brush Canyon Trail and Mount Bell Loop",
             file: "BrushCanyon.csv",
-            pos: {lat: 34.12501645871609,lng: -118.31391677586349} //34.12501645871609, -118.31391677586349
+            pos: {lat: 34.12501645871609,lng: -118.31391677586349}, //34.12501645871609, -118.31391677586349
+            alltrails: "https://www.alltrails.com/trail/us/california/brush-canyon-trail-and-mount-bell-loop?u=i&sh=sqfqqm"
         },
         {   name: "Observatory Trail Extended Loop",
             file: "ObservatoryTrail.csv" ,
-            pos: { lat: 34.12545624202727,lng: -118.31386991416954 } //34.12545624202727,-118.31386991416954
+            pos: { lat: 34.118595788327305,lng: -118.29437454983662 }, //34.118595788327305, -118.29437454983662
+            alltrails: "https://www.alltrails.com/trail/us/california/observatory-trail-extended-loop-trail?u=i&sh=sqfqqm"
+        },
+        {   name: "Fern Canyon, Hogback, North, Bill Eckert, and Upper Old Zoo Loop",
+            file: "UpperOldZooLoop.csv" ,
+            pos: { lat: 34.131043514209274,lng: -118.28348836098975 }, //34.131043514209274, -118.28348836098975
+            alltrails: "https://www.alltrails.com/trail/us/california/mt-hollywood-trail?u=i&sh=sqfqqm"
         },
     ];
     const [selectedOption, setSelectedOption] = useState(0); // Initial selected option
@@ -36,7 +43,7 @@ function Mainframe(){
         <hr></hr>
         <MyHikingMap csv={choices[selectedOption].file} mark={choices[selectedOption].pos}/>
         <hr></hr>
-        <h3><a href="https://www.alltrails.com/parks/us/california/griffith-park">More trails ...</a></h3>
+        <h4><a href={choices[selectedOption].alltrails}>More on Alltrails ...</a></h4>
         <p><a href="https://kcfamilyjourney.github.io/home/">Home</a> | <a href="https://forms.gle/ErqZysKrBFxnL5SV7">Contact Us</a></p>
     </>;
 }
