6433fe468a9d594032a39b7ef437bd8a05f1a1fe
bobkkblue <chienyec@gmail.com>
2025-12-26 10:45:12 -0800

    change path color

--- src/MyHikingMap.jsx
+++ src/MyHikingMap.jsx
@@ -14,7 +14,7 @@ const HikingRoute = ({ path }) => {
     const polyline = new google.maps.Polyline({
       path: path,
       geodesic: true,
-      strokeColor: 'rgba(242, 104, 12, 1)', // Red for the trail
+      strokeColor: 'rgba(34, 23, 233, 0.43)', // blue for the trail
       strokeOpacity: 1.0,
       strokeWeight: 3,
     });
